0bc79620990070d1dab685e4a95080817a63ee97
mana_kimura <m.i.l.l.wancan@gmail.com>
2017-09-01 08:01:53 +0000

    user registration

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -11,5 +11,11 @@
 |
 */
 
-Route::get('/', 'TasksController@index');
+Route::get('/', function () {
+    return view('welcome');
+});
 Route::resource('tasks', 'TasksController');
+
+// ユーザ登録
+Route::get('signup', 'Auth\AuthController@getRegister')->name('signup.get');
+Route::post('signup', 'Auth\AuthController@postRegister')->name('signup.post');
